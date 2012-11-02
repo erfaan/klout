3c79570c4379ecf6fa7d62a4875e1a924f0ea03a
Irfan Ahmad <erfaan@gmail.com>
2012-11-02 17:36:23 +0500

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -11,4 +11,15 @@ setup(
     license='LICENSE.txt',
     description='Minimalist Klout API interface.',
     long_description=open('README.rst').read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications',
+    ],
 )
