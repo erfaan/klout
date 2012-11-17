673273ee6b389d6caef120a195ca05cfa8b1ec10
Irfan Ahmad <erfaan@gmail.com>
2012-11-18 03:28:25 +0500

    Travis has a different nose version

--- _travis_setup.py
+++ _travis_setup.py
@@ -1,8 +1,8 @@
 import sys
 
-requirements = ['nose', 'unittest2']
+requirements = ['nose==1.1.2', 'unittest2']
 if sys.version_info >= (3, 0):
-    requirements = ['nose', 'unittest2py3k']
+    requirements = ['nose==1.1.2', 'unittest2py3k']
 
 f = open('requirements.txt' ,'w')
 for package in requirements:
