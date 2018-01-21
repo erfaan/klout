07aa018089bc80adb4d5d15314f0bc4d0972c1e6
Irfan <hello@i.com.pk>
2018-01-21 15:17:10 +0500

    Add coveralls.io config

--- _travis_setup.py
+++ _travis_setup.py
@@ -1,8 +1,8 @@
 import sys
 
-requirements = ['nose==1.1.2', 'unittest2']
+requirements = ['coveralls==1.2.0', 'coverage==4.4.2', 'unittest2']
 if sys.version_info >= (3, 0):
-    requirements = ['nose==1.1.2', 'unittest2py3k']
+    requirements = ['coveralls==1.2.0', 'coverage==4.4.2', 'unittest2py3k']
 
 f = open('requirements.txt', 'w')
 for package in requirements:
